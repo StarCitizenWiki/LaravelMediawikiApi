2d900268fa69866a55073450b2546b50de810ee3
FoXFTW <hannes@octofox.de>
2018-10-09 17:29:04 +0200

    :recycle: Refactor ApiErrorException to hold MediaWikiResponse

--- src/Exceptions/ApiErrorException.php
+++ src/Exceptions/ApiErrorException.php
@@ -8,8 +8,35 @@
 namespace StarCitizenWiki\MediaWikiApi\Exceptions;
 
 use Exception;
+use StarCitizenWiki\MediaWikiApi\Api\Response\MediaWikiResponse;
 
+/**
+ * Api Error occurred
+ */
 class ApiErrorException extends Exception
 {
+    /**
+     * @var \StarCitizenWiki\MediaWikiApi\Api\Response\MediaWikiResponse
+     */
+    private $response;
 
+    /**
+     * ApiErrorException constructor.
+     *
+     * @param \StarCitizenWiki\MediaWikiApi\Api\Response\MediaWikiResponse $response
+     */
+    public function __construct(MediaWikiResponse $response = null)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get the Guzzle Response
+     *
+     * @return \StarCitizenWiki\MediaWikiApi\Api\Response\MediaWikiResponse
+     */
+    public function getResponse(): MediaWikiResponse
+    {
+        return $this->response;
+    }
 }
